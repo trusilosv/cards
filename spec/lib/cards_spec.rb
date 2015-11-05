cd60c2e46bcc562dcf298b62e3ddef76ca42dbb5
Alexander Rjazantsev <ar@anahoret.com>
2015-11-05 13:11:05 +0300

    Add checking on created card.

--- spec/lib/cards_spec.rb
+++ spec/lib/cards_spec.rb
@@ -21,16 +21,31 @@ describe Cards do
   describe '.create_card' do
     subject { described_class.create_card(params) }
 
-    let(:params) { { name: "Name", description: "Description", tag_list: "design", author_id: author_id, project_id: project_id } }
+    let(:name) { "Name" }
+    let(:description) { "Description" }
+    let(:params) { { name: name, description: description, tag_list: "design", author_id: author_id, project_id: project_id } }
 
     it { expect{subject}.to change { Cards::Card.count }.by(1) }
     it { expect{subject}.to change { Cards::Tag.count }.by(1) }
     it { expect{subject}.to change { Cards::CardVersion.count }.by(1) }
 
     context 'response' do
-      it { expect(subject.name).to eq("Name") }
-      it { expect(subject.description).to eq("Description") }
+      it { expect(subject.name).to eq(name) }
+      it { expect(subject.description).to eq(description) }
       it { expect(subject.id).to be_present }
+    end
+
+    context 'created objects' do
+      subject! { described_class.create_card(params) }
+
+      context 'card' do
+        def card
+          Cards::Card.first
+        end
+
+        it { expect(card.name).to eq(name) }
+        it { expect(card.description).to eq(description) }
+      end
     end
   end
 end
