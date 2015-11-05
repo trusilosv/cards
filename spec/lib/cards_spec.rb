f7f6440b0b286f844790ed7f72b0c7cfc63b9110
Alexander Rjazantsev <ar@anahoret.com>
2015-11-05 13:41:14 +0300

    Add spec on find_card.

--- spec/lib/cards_spec.rb
+++ spec/lib/cards_spec.rb
@@ -3,27 +3,38 @@ require 'helper'
 describe Cards do
   let(:author_id) { 11111 }
   let(:project_id) { 22222 }
+  let(:name) { "Name" }
+  let(:description) { "Description" }
+  let(:params) { { name: name, description: description, tag_list: "design", author_id: author_id, project_id: project_id } }
+  let!(:first_card) { described_class.create_card(params) }
+  let!(:second_card) { described_class.create_card(params.merge(name: 'Second name', tag_list: 'bug')) }
 
   describe '.project_tags' do
-    it 'returns empty' do
-      expect(described_class.project_tags(1)).to be_empty
+    context "when project doesn't exist" do
+      it { expect(described_class.project_tags(1)).to be_empty }
     end
   end
 
   describe '.find_card' do
+    subject { described_class.find_card(some_id) }
+
     context 'when card does not exist' do
-      it 'returns null' do
-        expect(described_class.find_card(1)).to be_nil
-      end
+      let(:some_id) { 99999 }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when cards are present' do
+      let(:some_id) { first_card.id }
+
+      it { expect(subject.name).to eq(name) }
+      it { expect(subject.description).to eq(description) }
+      it { expect(subject.id).to be_present }
     end
   end
 
   describe '.create_card' do
-    subject { described_class.create_card(params) }
-
-    let(:name) { "Name" }
-    let(:description) { "Description" }
-    let(:params) { { name: name, description: description, tag_list: "design", author_id: author_id, project_id: project_id } }
+    subject { described_class.create_card(params.merge(tag_list: "new_tag")) }
 
     it { expect{subject}.to change { Cards::Card.count }.by(1) }
     it { expect{subject}.to change { Cards::Tag.count }.by(1) }
